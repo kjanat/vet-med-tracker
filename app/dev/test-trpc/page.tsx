88d8bc17c137fe583bab8e2a9d9b498e0517d718
Kaj Kowalski <info@kajkowalski.nl>
2025-09-30 18:01:42 +0200

    fix: remove Error type annotation from onError callback

--- app/dev/test-trpc/page.tsx
+++ app/dev/test-trpc/page.tsx
@@ -286,7 +286,7 @@ function CreateHouseholdForm({ onSuccess }: { onSuccess: () => void }) {
       onSuccess();
       setNewHouseholdName("");
     },
-    onError: (error: Error) => {
+    onError: (error) => {
       console.error("Error creating household:", error);
     },
   });
